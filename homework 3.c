dd3a22ed2247572fdd5686b837f211a6f4a5fdab
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-01-03 14:00:32 +1300

    Update homework 3.c

--- homework 3.c
+++ homework 3.c
@@ -2,17 +2,19 @@
 #include <stdio.h>
 int main()
 {
-int n,ez,a;
-do{
-    printf("value of n: ");
-    scanf("%i",&n);
-  }while(n>15||n<=0);
-for(ez=0,a=0;ez<n&&a<n;){
-    a++;
-  if(a%2==0){
-ez=a;
-      printf("%i\n",ez);
-  }
-
-      }
+    int n, ez = 1;
+    do
+    {
+        printf("value of n: ");
+        scanf("%i", &n);
+    } while (n > 15 || n <= 0);
+    
+    for (int a = 1;a < n; a++)
+    {
+        if (a % 2 == 0)
+        {
+            ez *= a;
+        }
     }
+    printf("%i\n", ez);
+}
