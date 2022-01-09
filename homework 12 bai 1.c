8d2ade57546f2d80d93fd6fd3db55df5af3ddf10
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-01-10 11:25:35 +1300

    Create homework 12 bai 1.c

---
+++ homework 12 bai 1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+int main(void)
+{
+    char s[1000];
+    int yeMan=0;
+    printf("enter string: ");
+    gets(s);
+    for(int i=strlen(s);i>=0;i--){
+        printf("%c",s[i]);
+    }
+}
