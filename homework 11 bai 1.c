1b8ce645da570591484a15a74aa9f50edd94e78f
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-01-15 13:10:46 +1300

    Update homework 11 bai 1.c

--- homework 11 bai 1.c
+++ homework 11 bai 1.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 int main(void)
 {
-    char s1[81], s2[81];
+    char s1[81], s2[81],s[82];
     printf("enter string 1:");
     gets(s1);
     printf("enter string 2:");
     gets(s2);
-    printf("%s%s", s1, s2);
-    return 0;
+    strcat(s,s1);
+    strcat(s,s2);
+    printf("%s",s);
 }
