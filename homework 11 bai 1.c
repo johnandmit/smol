c1df0b80609ce68e402b2cc88db321d9a4b75f82
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-01-17 21:01:43 +1300

    Update homework 11 bai 1.c

--- homework 11 bai 1.c
+++ homework 11 bai 1.c
@@ -4,9 +4,9 @@ int main(void)
 {
     char s1[81], s2[81],s[82];
     printf("enter string 1:");
-    gets(s1);
+    fgets(s1,sizeof(s1),stdin);
     printf("enter string 2:");
-    gets(s2);
+    fgets(s2,sizeof(s1),stdin);
     strcat(s,s1);
     strcat(s,s2);
     printf("%s",s);
