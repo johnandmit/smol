6295984a993fc81c70c311e56e960e49a37a7abf
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-01-18 15:34:43 +1300

    Update homework 12 bai 7.c

--- homework 12 bai 7.c
+++ homework 12 bai 7.c
@@ -4,15 +4,16 @@
 #include <math.h>
 int main(void)
 {
-    char s[1000];
+    char s[80],e='$',a;
     int yeman = 0, ye = 0;
     printf("enter string: ");
-    gets(s);
-
+    fgets(s,sizeof(s),stdin);
+    a=s[0];
     for (int i = 0; i < strlen(s); i++)
     {
-        if(s[0]==s[i]){
-            printf("$");
+        if(a==s[i]){
+            s[i]=e;
+            printf("%c",s[i]);
         }
         else{
             printf("%c",s[i]);
