dc6e21a457eba9d79f46c6868657e9dad0fb7323
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-01-19 15:17:27 +1300

    Create homework 13 bai 4.c

---
+++ homework 13 bai 4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+int is_prime(int n)
+{
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+int main()
+{
+    int n = 4;
+    printf("%i", is_prime(n));
+}
