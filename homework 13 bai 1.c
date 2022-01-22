6af847e97f2376b6d2b39d358718b62eee069182
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-01-23 11:19:35 +1300

    Update homework 13 bai 1.c

--- homework 13 bai 1.c
+++ homework 13 bai 1.c
@@ -24,93 +24,36 @@ int sort(int n, float num)
         return 1;
     }
 }
-void text(float num,int ez)
+void text(float num,int ez,int temp)
 {
+    
+    if(temp!=10){
     if (ez == 1)
-    {
-        if (num == 100)
-            printf("mot tram ");
-        if (num == 10)
-            printf("muoi ");
-        if (num == 1)
-            printf("mot ");
-    }
+        printf("mot ");}
     if (ez == 2)
-    {
-        if (num == 100)
-            printf("hai tram ");
-        if (num == 10)
-            printf("hai muoi ");
-        if (num == 1)
-            printf("hai ");
-    }
+        printf("hai ");
     if (ez == 3)
-    {
-        if (num == 100)
-            printf("ba tram ");
-        if (num == 10)
-            printf("ba muoi ");
-        if (num == 1)
-            printf("ba ");
-    }
+        printf("ba ");
     if (ez == 4)
-    {
-        if (num == 100)
-            printf("bon tram ");
-        if (num == 10)
-            printf("bon muoi ");
-        if (num == 1)
-            printf("bon ");
-    }
+        printf("bon ");
     if (ez == 5)
-    {
-        if (num == 100)
-            printf("nam tram ");
-        if (num == 10)
-            printf("nam muoi ");
-        if (num == 1)
-            printf("nam ");
-    }
+        printf("nam ");
     if (ez == 6)
-    {
-        if (num == 100)
-            printf("sau tram ");
-        if (num == 10)
-            printf("sau muoi ");
-        if (num == 1)
-            printf("sau ");
-    }
+        printf("sau ");
     if (ez == 7)
-    {
-        if (num == 100)
-            printf("bay tram ");
-        if (num == 10)
-            printf("bay muoi ");
-        if (num == 1)
-            printf("bay ");
-    }
+        printf("bay ");
     if (ez == 8)
-    {
-        if (num == 100)
-            printf("tam tram ");
-        if (num == 10)
-            printf("tam muoi ");
-        if (num == 1)
-            printf("tam ");
-    }
+        printf("tam ");
     if (ez == 9)
-    {
-        if (num == 100)
-            printf("chin tram ");
-        if (num == 10)
-            printf("chin muoi ");
-        if (num == 1)
-            printf("chin ");
-    }
+        printf("chin ");
+    if (num == 100)
+        printf("tram ");
+    if (num == 10)
+        printf("muoi ");
 }
 int main()
 {
-    int n, e,ez,temp;
+    int n, e,ez,temp,a;
     float num;
     printf("enter number: ");
     scanf("%i", &n);
@@ -123,32 +66,35 @@ int main()
     {
         num = 100;
         ez=ez/num;
-        text(num,ez);
+        text(num,ez,temp);
         num = 10;
         ez=ez*100;
         ez=temp-ez;
-        temp=ez;
+        a=ez;
         ez=ez/10;
-        text(num,ez);
+        temp=ez*10;
+        text(num,ez,temp);
         num = 1;
         ez=ez*10;
-        ez=temp-ez;
-        text(num,ez);
+        ez=a-ez;
+        text(num,ez,temp);
     }
     if (e == 2)
     {
+        a=ez;
         num = 10;
         ez=ez/num;
-        text(num,ez);
+        temp=ez*10;
+        text(num,ez,temp);
         num = 1;
         ez=ez*10;
-        ez=temp-ez;
-        text(num,ez);
+        ez=a-ez;
+        text(num,ez,temp);
     }
     if (e == 1)
     {
         num = 1;
         ez=ez/num;
-        text(num,ez);
+        text(num,ez,temp);
     }
 }
