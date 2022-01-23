f4eaae6f9c56ef60454a8fa8ea57c843a2ad28fa
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-01-23 16:26:59 +1300

    Update homewrok 12 bai 8.c

--- homewrok 12 bai 8.c
+++ homewrok 12 bai 8.c
@@ -4,25 +4,26 @@
 #include <math.h>
 int main(void)
 {
-    char s[1000],s1[1000];
-    int yes=0;
+    char s[1000], s1[1000];
+    int yes = 1;
     printf("enter s: ");
     gets(s);
     printf("enter s1: ");
     gets(s1);
-    for(int i=0;i<strlen(s1);i++){
-        if(s1[i]!=s[i]){
-            yes=0;
+    for (int i = 0; i < strlen(s1); i++)
+    {
+        if (s1[i] != s[i])
+        {
+            yes = 0;
             break;
         }
-        if(s1[i]=s[i]){
-            yes=1;
-        }
     }
-    if(yes==1){
-        printf("s1 bat dau = s");
+    if (yes == 1)
+    {
+        printf("s bat dau = s1");
     }
-    else{
-        printf("s1 khong bat dau =s");
+    else
+    {
+        printf("s khong bat dau =s1");
     }
 }
