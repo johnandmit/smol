9d7f22620004448e77f4f7b4b101b7ef15122b56
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-01-23 16:46:48 +1300

    Update homework 13 bai 2b.c

--- homework 13 bai 2b.c
+++ homework 13 bai 2b.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
-int get_max(int a, int b,int c)
+int get_max(int a, int b, int c)
 {
-    if (a > b&&a>c)
+    if (a > b && a > c)
         return a;
-    else if(b>c&&b>a)
+    else if (b > c && b > a)
         return b;
     else
         return c;
 }
-int get_min(int a, int b,int c)
+int get_min(int a, int b, int c)
 {
-    if (a < b&&a<c)
+    if (a < b && a < c)
         return a;
-    else if(b<c&&b<a)
+    else if (b < c && b < a)
         return b;
     else
         return c;
 }
 int main(void)
 {
-    int a=1,b=13,c=1,a1,b1;
-    a1=get_max(a,b,c);
-    b1=get_min(a,b,c);
-    printf("%i\n%i",a1,b1);
+    int a = 15, b = 10, c = 5, a1, b1;
+    a1 = get_max(a, b, c);
+    b1 = get_min(a, b, c);
+    printf("%i\n%i", a1, b1);
 }
