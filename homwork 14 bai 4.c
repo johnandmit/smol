4414b889aecdf881fc17bc1199d911a10599b574
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-01-23 14:29:58 +1300

    Update homwork 14 bai 4.c

--- homwork 14 bai 4.c
+++ homwork 14 bai 4.c
@@ -8,19 +8,15 @@ int find_x(int a[], int n, int key)
             return i;
         }
         if(i==n-1){
-            return 0;
+            return -1;
         }
     }
 }
 int main()
 {
-    int a[8] = {3, 2, 7, 1, 9, 2, 8, 5},key=1;
-    if (find_x(a, 8, key) == 0)
-    {
+    int a[8] = {3, 2, 7, 1, 9, 2, 8, 5},key=3;
+    if (find_x(a, 8, key) == -1)
         printf("key is not in a");
-    }
     else
-    {
         printf("the place we found key is in a =%i", find_x(a, 8, key));
-    }
 }
