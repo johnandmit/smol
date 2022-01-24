2f6541454dddb77ae78fe94087e162fe4b53bfac
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-01-24 18:40:52 +1300

    Create homewrok 15 bai 3.c

---
+++ homewrok 15 bai 3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+int ezcheck(char s[],char e[])
+{
+    if (strcmp(s, e)==0)
+        return 1;
+    else
+        return 0;
+}
+int main()
+{
+    char s[50],e[50];
+    printf("enter s: ");
+    gets(s);
+    strcpy(e,s);
+    strrev(e);
+    printf("%i",ezcheck(s,e));
+}
