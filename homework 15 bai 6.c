47ea54188061e3ad24c0cd8a8cc9ecd0fd81abfe
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-01-27 15:47:59 +1300

    Create homework 15 bai 6.c

---
+++ homework 15 bai 6.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+void ezcheck(int n, int s[])
+{
+    int i=0,yes;
+    for (int e = 0; e < n; e++)
+    {
+        yes = 1;
+        for (int a = 2; a < s[e];a++)
+        {
+            if(s[e]%a==0)
+                yes=0;
+        }
+        if(yes==1){
+            i++;
+        }
+    }
+    printf("%i ",i);
+}
+int main()
+{
+    int n, s[80], x;
+    printf("enter n: ");
+    scanf("%i", &n);
+    for (int e = 0; e < n; e++)
+    {
+        printf("enter x: ");
+        scanf("%i", &x);
+        s[e] = x;
+    }
+    ezcheck(n, s);
+}
