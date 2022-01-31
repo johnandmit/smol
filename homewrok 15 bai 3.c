8d8302fb2a8a571429bcb78f0279aaff1dfcbe3e
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-01-31 16:08:45 +1300

    Update homewrok 15 bai 3.c

--- homewrok 15 bai 3.c
+++ homewrok 15 bai 3.c
@@ -2,7 +2,7 @@
 #include <string.h>
 int ezcheck(char word1[],char word2[])
 {
-    if (strcmp(word1, word2))
+    if (strcmp(word1, strrev(word2)))
         return 0;
     else
         return 1;
@@ -10,10 +10,9 @@ int ezcheck(char word1[],char word2[])
 int main()
 {
     char word1[50],word2[50];
-    printf("enter s: ");
+    printf("enter word1: ");
     gets(word1);
-    printf("enter e: ");
+    printf("enter word2: ");
     gets(word2);
-    strrev(word2);
     printf("%i",ezcheck(word1,word2));
 }
