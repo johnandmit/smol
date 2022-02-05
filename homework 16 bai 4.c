e1cfd9a5a64a0a0dabf28dde91a86f4b3e0058d2
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-02-05 14:05:18 +1300

    Update homework 16 bai 4.c

--- homework 16 bai 4.c
+++ homework 16 bai 4.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 void check(int n, int m)
 {
-    int p=0;
+    int p=0,sum=0;
     for (int i = 1; i <= n; i++)
     {
         if (n % i==0&&m%i==0)
         {
             p++;
+            sum+=i;
         }
     }
     printf("%i ",p);
+    printf("\nsum = %i",sum);
 }
 int main()
 {
-    int n = 6, m = 3;
+    int n, m;
+    printf("enter n: ");
+    scanf("%i",&n);
+    printf("enter m: ");
+    scanf("%i",&m);
     check(n, m);
 }
