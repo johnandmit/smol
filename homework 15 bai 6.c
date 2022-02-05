af5de63736a319e6df5ba59421c751a788d5844b
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-02-05 17:55:35 +1300

    Update homework 15 bai 6.c

--- homework 15 bai 6.c
+++ homework 15 bai 6.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <math.h>
-void enterX(int n,int x,int s[]){
+void enterX(int n, int x, int s[])
+{
     for (int e = 0; e < n; e++)
     {
         printf("enter x: ");
@@ -8,25 +9,27 @@ void enterX(int n,int x,int s[]){
         s[e] = x;
     }
 }
-int isPrime(int n){
-    if(n<2)
+int isPrime(int n)
+{
+    if (n < 2)
         return 0;
-    else{
-        for (int a = 2; a < n;a++)
+    else
+    {
+        for (int a = 2; a < n; a++)
         {
-            if(n%a==0)
+            if (n % a == 0)
                 return 0;
         }
         return 1;
-        }
+    }
 }
 void ezcheck(int n, int s[])
 {
-    int i=0;
+    int i = 0;
     for (int e = 0; e < n; e++)
     {
-        if(isPrime(s[e])==1)
-            printf("%i ",s[e]);
+        if (isPrime(s[e]) == 1)
+            printf("%i ", s[e]);
     }
 }
 int main()
@@ -34,6 +37,6 @@ int main()
     int n, s[80], x;
     printf("enter n: ");
     scanf("%i", &n);
-    enterX(n,x,s);
+    enterX(n, x, s);
     ezcheck(n, s);
 }
