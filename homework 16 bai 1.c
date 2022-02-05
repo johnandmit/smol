081f8369d74019797df4355b65750e7d9d30cb6a
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-02-05 13:27:43 +1300

    Update homework 16 bai 1.c

--- homework 16 bai 1.c
+++ homework 16 bai 1.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 int is_sorted(int n[])
 {
+    int count=0;
     for(int i=0;i<9;i++){
         if(n[i]>n[i+1])
-            return 0;
+            count++;
+    }
+    if(count==9||count>9){
+        return 0;
     }
     return 1;
 }
 int main()
 {
-    int n[10]={0,1,2,3,4,5,6,7,8,9};
+    int n[10]={9,8,7,6,5,4,3,2,1,0};
     printf("%i ",is_sorted(n));
 }
