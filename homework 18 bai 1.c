2ade1c158281b350e76296a88488700c976364f7
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-02-07 16:20:03 +1300

    Update homework 18 bai 1.c

--- homework 18 bai 1.c
+++ homework 18 bai 1.c
@@ -1,57 +1,27 @@
 #include <stdio.h>
-#include <math.h>
-struct date
-{
-    int month;
-    int day;
-    int year;
+#include <string.h>
+struct Hoso{
+    char HoTen[40];
+    float Diem;
+    char Loai[10];
 };
-int main()
-{
-    struct date a;
-    int yes = 0;
-    printf("enter day: ");
-    scanf("%i", &a.day);
-    printf("enter month: ");
-    scanf("%i", &a.month);
-    printf("enter year: ");
-    scanf("%i", &a.year);
-    printf("day = %i\nmonth = %i\nyear = %i\n", a.day, a.month, a.year);
-    if (a.year % 100 == 0 && a.year % 400 == 0 && a.month == 2)
-    {
-        yes = 1;
+int main(){
+    struct Hoso a[30];
+    int n,i;
+    printf("enter n: "); scanf("%i",&n);
+    for(i=0;i<n;i++){
+        printf("enter Ho ten: "); scanf("%s",&a[i].HoTen);
+        printf("enter diem: "); scanf("%f",&a[i].Diem);
+        float crntDiem = a[i].Diem;
+        if(crntDiem >= 9)
+            strcpy(a[i].Loai, "Gioi");
+        else if(crntDiem >= 7)
+            strcpy(a[i].Loai, "Kha");
+        else if(crntDiem >= 5)
+            strcpy(a[i].Loai, "Trung binh");
+        else
+            strcpy(a[i].Loai, "Khong Dat");
     }
-    if (a.year % 4 == 0 && a.year % 100 != 0 && a.year % 400 != 0 && a.month == 2)
-    {
-        yes = 1;
-    }
-    if ((a.month == 1 && a.day > 32) || (a.month == 2 && (a.day > 30)||a.day==29&&yes==0) || (a.month == 3 && a.day > 32) || (a.month == 4 && a.day > 31) || (a.month == 5 && a.day > 32) || (a.month == 6 && a.day > 31) || (a.month == 7 && a.day > 32) || (a.month == 8 && a.day > 32) || (a.month == 9 && a.day > 31) || (a.month == 10 && a.day > 32) || (a.month == 11 && a.day > 31) || (a.month == 12 && a.day > 32) || a.month > 12)
-    {
-        printf("learn more");
-        return 0;
-    }
-    if ((a.month == 1 && a.day == 32) || (a.month == 2 && a.day >= 28) || (a.month == 3 && a.day == 32) || (a.month == 4 && a.day == 31) || (a.month == 5 && a.day == 32) || (a.month == 6 && a.day == 31) || (a.month == 7 && a.day == 32) || (a.month == 8 && a.day == 32) || (a.month == 9 && a.day == 31) || (a.month == 10 && a.day == 32) || (a.month == 11 && a.day == 31) || (a.month == 12 && a.day == 32))
-    {
-        if (a.day == 28 && yes == 1)
-        {
-            a.month = 2;
-            a.day = 29;
-        }
-        else if (a.month != 12)
-        {
-            a.month += 1;
-            a.day = 1;
-        }
-        if (a.month == 12)
-        {
-            a.year += 1;
-            a.month = 1;
-            a.day = 1;
-        }
-    }
-    else
-    {
-        a.day++;
-    }
-    printf("\nday = %i\nmonth = %i\nyear = %i\n", a.day, a.month, a.year);
+    for(int j=0;j<i;j++)
+        printf("%s la loai %s\n",a[j].HoTen,a[j].Loai);
 }
