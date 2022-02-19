0ed825ed27b414d6544d44162561b8ac99ead446
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-02-19 20:44:52 +1300

    Update homework 19 bai 2.c

--- homework 19 bai 2.c
+++ homework 19 bai 2.c
@@ -15,20 +15,14 @@ while(a!=b)
 }
 void multi(int a,int b,int c,int d)
 {
-    int e,f,m;
-    e=a*c;
-    f=b*d;
-    m=lowest_multi(e,f);
-    e=e/m;
-    f=f/m;
-    printf("%i/%i",e,f);
+    int m;
+    m=lowest_multi(a*c,b*d);
+    printf("%i/%i",a*c/m,b*d/m);
 }
 void plus_minus(int e,int f){
     int c;
     c=lowest_multi(e,f);
-    e/=c;
-    f/=c;
-    printf("%i/%i",e,f);
+    printf("%i/%i",e/c,f/c);
 }
 struct phan_so
 {
@@ -37,30 +31,30 @@ struct phan_so
 };
 int main(){
     int n;
-    struct phan_so a[2];
+    struct phan_so a,b;
     printf("enter tu so cua phan so thu nhat: ");
-    scanf("%i",&a[0].tu_so);
+    scanf("%i",&a.tu_so);
     printf("enter tu so cua phan so thu nhat: ");
-    scanf("%i",&a[0].mau_so);
+    scanf("%i",&a.mau_so);
     printf("enter tu so cua phan so thu hai: ");
-    scanf("%i",&a[1].tu_so);
+    scanf("%i",&b.tu_so);
     printf("enter tu so cua phan so thu hai: ");
-    scanf("%i",&a[1].mau_so);
+    scanf("%i",&b.mau_so);
     printf("enter 1 for plus\nenter 2 for minus\nenter 3 for devision\nenter 4 for multiplication\nenter here: ");
     scanf("%i",&n);
     switch(n)
     {
         case 1:
-            plus_minus(a[0].tu_so*a[1].mau_so+a[1].tu_so*a[0].mau_so,a[0].mau_so*a[1].mau_so);
+            plus_minus(a.tu_so*b.mau_so+b.tu_so*a.mau_so,a.mau_so*b.mau_so);
             break;
         case 2:
-            plus_minus(a[0].tu_so*a[1].mau_so-a[1].tu_so*a[0].mau_so,a[0].mau_so*a[1].mau_so);
+            plus_minus(a.tu_so*b.mau_so-b.tu_so*a.mau_so,a.mau_so*b.mau_so);
             break;
         case 3:
-            multi(a[0].tu_so,a[0].mau_so,a[1].mau_so,a[1].tu_so);
+            multi(a.tu_so,a.mau_so,b.mau_so,b.tu_so);
             break;
         case 4:
-            multi(a[0].tu_so,a[0].mau_so,a[1].tu_so,a[1].mau_so);
+            multi(a.tu_so,a.mau_so,b.tu_so,b.mau_so);
             break;
         default:
             break;
