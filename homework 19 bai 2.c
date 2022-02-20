e6c9d1b6914d99536b4139df4528f061cc648c17
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-02-20 13:20:34 +1300

    Update homework 19 bai 2.c

--- homework 19 bai 2.c
+++ homework 19 bai 2.c
@@ -1,60 +1,73 @@
 #include <stdio.h>
 #include <math.h>
-int lowest_multi(int a,int b){
-    a=abs(a);
-    b=abs(b);
-while(a!=b)
-    {
-        if(a>b){
-            a-=b;
-        }
-        else
-            b-=a;
-    }
-    return a;
-}
-void multi(int a,int b,int c,int d)
-{
-    int m;
-    m=lowest_multi(a*c,b*d);
-    printf("%i/%i",a*c/m,b*d/m);
-}
-void plus_minus(int e,int f){
-    int c;
-    c=lowest_multi(e,f);
-    printf("%i/%i",e/c,f/c);
-}
 struct phan_so
 {
     int tu_so;
     int mau_so;
 };
-int main(){
+struct phan_so simplify(struct phan_so p){
+    int a=abs(p.tu_so),b=abs(p.mau_so);
+    while(a!=b)
+        if(a>b)
+            a-=b;
+        else
+            b-=a;
+    p.tu_so /= a;
+    p.mau_so /= a;
+    return p;
+}
+struct phan_so plus(struct phan_so a, struct phan_so b){
+    struct phan_so c;
+    c.tu_so=a.tu_so*b.mau_so+b.tu_so*a.mau_so;
+    c.mau_so=b.mau_so*a.mau_so;
+    return simplify(c);
+}
+struct phan_so multiply(struct phan_so a, struct phan_so b){
+    struct phan_so c;
+    c.tu_so=a.tu_so*b.tu_so;
+    c.mau_so=a.mau_so*b.mau_so;
+    return simplify(c);
+}
+int main()
+{
     int n;
-    struct phan_so a,b;
+    struct phan_so a, b;
     printf("enter tu so cua phan so thu nhat: ");
     scanf("%i",&a.tu_so);
-    printf("enter tu so cua phan so thu nhat: ");
+    printf("enter mau so cua phan so thu nhat: ");
     scanf("%i",&a.mau_so);
     printf("enter tu so cua phan so thu hai: ");
     scanf("%i",&b.tu_so);
-    printf("enter tu so cua phan so thu hai: ");
+    printf("enter mau so cua phan so thu hai: ");
     scanf("%i",&b.mau_so);
-    printf("enter 1 for plus\nenter 2 for minus\nenter 3 for devision\nenter 4 for multiplication\nenter here: ");
+    printf("enter 1 for plus\nenter 2 for minus\nenter 3 for multiplication\nenter 4 for devision\nenter here: ");
     scanf("%i",&n);
+    struct phan_so c;
     switch(n)
     {
         case 1:
-            plus_minus(a.tu_so*b.mau_so+b.tu_so*a.mau_so,a.mau_so*b.mau_so);
+            printf("%i/%i+%i/%i=",a.tu_so,a.mau_so,b.tu_so,b.mau_so);
+            c = plus(a, b);
+            printf("%i/%i", c.tu_so, c.mau_so);
             break;
         case 2:
-            plus_minus(a.tu_so*b.mau_so-b.tu_so*a.mau_so,a.mau_so*b.mau_so);
+            printf("%i/%i-%i/%i=",a.tu_so,a.mau_so,b.tu_so,b.mau_so);
+            b.tu_so = -b.tu_so;
+            c = plus(a, b);
+            printf("%i/%i", c.tu_so, c.mau_so);
             break;
         case 3:
-            multi(a.tu_so,a.mau_so,b.mau_so,b.tu_so);
+            printf("%i/%i*%i/%i=",a.tu_so,a.mau_so,b.tu_so,b.mau_so);
+            c = multiply(a, b);
+            printf("%i/%i", c.tu_so, c.mau_so);
             break;
         case 4:
-            multi(a.tu_so,a.mau_so,b.tu_so,b.mau_so);
+            printf("%i/%i/%i/%i=",a.tu_so,a.mau_so,b.tu_so,b.mau_so);
+            int temp = b.tu_so;
+            b.tu_so = b.mau_so;
+            b.mau_so = temp;
+            c = multiply(a, b);
+            printf("%i/%i", c.tu_so, c.mau_so);
             break;
         default:
             break;
