9a62a3099cd5fba987709b0e06965069738af657
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-02-21 19:52:35 +1300

    Update homework 19 bai 2.c

--- homework 19 bai 2.c
+++ homework 19 bai 2.c
@@ -22,14 +22,14 @@ struct phan_so plus(struct phan_so a, struct phan_so b)
     struct phan_so c;
     c.tu_so = a.tu_so * b.mau_so + b.tu_so * a.mau_so;
     c.mau_so = b.mau_so * a.mau_so;
-    return simplify(c);
+    return c;
 }
 struct phan_so multiply(struct phan_so a, struct phan_so b)
 {
     struct phan_so c;
     c.tu_so = a.tu_so * b.tu_so;
     c.mau_so = a.mau_so * b.mau_so;
-    return simplify(c);
+    return c;
 }
 struct phan_so minus(struct phan_so a, struct phan_so b)
 {
@@ -45,7 +45,7 @@ struct phan_so divide(struct phan_so a, struct phan_so b)
 }
 int main()
 {
-    int n,e;
+    int n, e;
     struct phan_so a, b;
     printf("enter tu so cua phan so thu nhat: ");
     scanf("%i", &a.tu_so);
@@ -58,34 +58,32 @@ int main()
     struct phan_so c;
     do
     {
-        printf("enter 1 for plus\nenter 2 for minus\nenter 3 for devide\nenter 4 for multiplication\nenter here: ");
+        printf("enter 1 for plus\nenter 2 for minus\nenter 3 for multiplication\nenter 4 for devision\nenter here: ");
         scanf("%i", &n);
         switch (n)
         {
         case 1:
             printf("%i/%i+%i/%i=", a.tu_so, a.mau_so, b.tu_so, b.mau_so);
             c = plus(a, b);
-            printf("%i/%i\n", c.tu_so, c.mau_so);
             break;
         case 2:
             printf("%i/%i-%i/%i=", a.tu_so, a.mau_so, b.tu_so, b.mau_so);
             c = minus(a, b);
-            printf("%i/%i\n", c.tu_so, c.mau_so);
             break;
         case 3:
-            printf("(%i/%i)/(%i/%i)=", a.tu_so, a.mau_so, b.tu_so, b.mau_so);
-            c = divide(a, b);
-            printf("%i/%i\n", c.tu_so, c.mau_so);
+            printf("%i/%i/%i/%i=", a.tu_so, a.mau_so, b.tu_so, b.mau_so);
+            c = multiply(a, b);
             break;
         case 4:
             printf("%i/%i*%i/%i=", a.tu_so, a.mau_so, b.tu_so, b.mau_so);
-            c = multiply(a, b);
-            printf("%i/%i\n", c.tu_so, c.mau_so);
+            c = divide(a, b);
             break;
         default:
             break;
         }
+        c = simplify(c);
+        printf("%i/%i\n", c.tu_so, c.mau_so);
         printf("enter 1 to caculate again\nenter 0 to exit program\n enter here: ");
-        scanf("%i",&e);
-    }while(e==1);
+        scanf("%i", &e);
+    } while (e == 1);
 }
