f1b9eeb9f9f54e8cef9dbe692570d68b37f022eb
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-03-29 19:40:29 +1300

    Create homework 30 bai 1.c

---
+++ homework 30 bai 1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int n, sum = 0;
+	printf("enter n: ");
+	scanf("%i", &n);
+	for (int count = 1; count <= n; count++)
+	{
+		if (count % 2 == 0)
+		{
+			sum += count;
+			printf("%i\n", count);
+		}
+	}
+	printf("sum = %i", sum);
+}
