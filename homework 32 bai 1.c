9f62bc37bd2773dc6f794b464cf4c95e5f79f6d3
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-04-08 15:49:46 +1200

    Update homework 32 bai 1.c

--- homework 32 bai 1.c
+++ homework 32 bai 1.c
@@ -2,34 +2,31 @@
 #include <math.h>
 int main()
 {
-	while (1)
+	int a, b, c, delta, x_1, x_2, x;
+	do
 	{
-		int a, b, c, delta, x_1, x_2, x;
-		do
-		{
-			printf("enter a: ");
-			scanf("%i", &a);
-		} while (a == 0);
-		printf("enter b: ");
-		scanf("%i", &b);
-		printf("enter c: ");
-		scanf("%i", &c);
-		delta = b * b - 4 * a * c;
-		if (delta > 0)
-		{
-			x_1 = (-b - sqrt(delta) / (2 * a));
-			x_2 = (-b + sqrt(delta) / (2 * a));
-			printf("%i\n", x_1);
-			printf("%i\n", x_2);
-		}
-		else if (delta == 0)
-		{
-			x = -b / (2 * a);
-			printf("%i", x);
-		}
-		else
-		{
-			printf("phuong trinh vo ngiem ");
-		}
+		printf("enter a: ");
+		scanf("%i", &a);
+	} while (a == 0);
+	printf("enter b: ");
+	scanf("%i", &b);
+	printf("enter c: ");
+	scanf("%i", &c);
+	delta = b * b - 4 * a * c;
+	if (delta > 0)
+	{
+		x_1 = (-b - sqrt(delta) / (2 * a));
+		x_2 = (-b + sqrt(delta) / (2 * a));
+		printf("%i\n", x_1);
+		printf("%i\n", x_2);
+	}
+	else if (delta == 0)
+	{
+		x = -b / (2 * a);
+		printf("%i", x);
+	}
+	else
+	{
+		printf("phuong trinh vo ngiem ");
 	}
 }
