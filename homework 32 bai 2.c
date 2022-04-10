45be9f563f2db30b16c5d919b2537928ae32f249
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-04-10 16:07:44 +1200

    Update homework 32 bai 2.c

--- homework 32 bai 2.c
+++ homework 32 bai 2.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int FindSumEquation(int a[], int n, int x)
+{
+	for (int i = 0; i < n - 1; i++)
+	{
+		if (x == (a[i] + a[i + 1]))
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
 int main()
 {
 	int A[1000], x;
@@ -6,15 +20,10 @@ int main()
 	{
 		A[i] = rand() % 100;
 	}
-	
+
 	printf("enter x: ");
 	scanf("%i", &x);
 
-	for (int i = 0; i < 1000; i++)
-	{
-		if(x==A[i]&&x==A[i+1])
-		{
-			printf("\n%i , %i\n",i,i);
-		}
-	}
+	int i = FindSumEquation(A, 1000, x);
+	printf("%i", i);
 }
