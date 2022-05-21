12cf3d7c248410a9d0fd169cd5ba2bc5ef934607
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-05-21 20:22:15 +1200

    Update homework 41 bai 1 version 2.c

--- homework 41 bai 1 version 2.c
+++ homework 41 bai 1 version 2.c
@@ -1,1 +1,32 @@
-
+#include <stdio.h>
+int costCaculator(int kWh)
+{
+	int cost;
+	if (kWh < 100)
+	{
+		cost = kWh * 1600;
+	}
+	else if (kWh < 300)
+	{
+		cost = 160000 + (kWh - 100) * 2000;
+	}
+	else
+	{
+		cost = 560000 + (kWh - 300) * 3000;
+	}
+	return cost;
+}
+int main()
+{
+	char Name[30];
+	int kWh, cost;
+	FILE *fp = fopen("C:\\Users\\minhp\\source\\repos\\Stuff\\file_electric_2205.csv", "r");
+	FILE *fp2 = fopen("C:\\Users\\minhp\\source\\repos\\Stuff\\output_electric_2205.csv", "w");
+	while (fscanf(fp, "%s %d", Name, &kWh) != EOF)
+	{
+		cost = costCaculator(kWh);
+		fprintf(fp2, "%s, %d\n", Name, cost);
+	}
+	fclose(fp);
+	fclose(fp2);
+}
