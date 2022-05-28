c4b0c754a6bb608c9070f8f348d37c6e6bcbcd81
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-05-28 16:49:56 +1200

    Create homework 43 bai 1.c

---
+++ homework 43 bai 1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+void check(int a, int b, int c)
+{
+	if (a + b > c && a + c > b && c + b > a)
+	{
+		float perimeter = (a + b + c) / 2;
+		float area = sqrt(perimeter * (perimeter - a) * (perimeter - b) * (perimeter - c));
+		printf("area: %f\nperimeter: %f", area, perimeter);
+	}
+	else
+	{
+		printf("a, b, c are not sides of a triangle");
+	}
+}
+int main()
+{
+	int a, b, c;
+	printf("enter a: ");
+	scanf("%i", &a);
+	printf("enter b: ");
+	scanf("%i", &b);
+	printf("enter c: ");
+	scanf("%i", &c);
+	check(a, b, c);
+}
