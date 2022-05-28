d97350c5a296164a16689f1c735bdce60d4bb71a
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-05-28 17:03:50 +1200

    Create homework 43 bai 2.c

---
+++ homework 43 bai 2.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <math.h>
+void FindS(int n)
+{
+	float s = 1;
+	for (float i = 2; i <= n; i++)
+	{
+		s += (i - 1) / i;
+	}
+	printf("%f\n", s);
+}
+int IsPrime(int n)
+{
+	for (int i = 2; i < sqrt(n); i++)
+	{
+		if(n%i == 0)
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+int main()
+{
+	int n;
+	do
+	{
+		printf("enter n: ");
+		scanf("%i", &n);
+	} while (n <= 5);
+	FindS(n);
+	printf("enter n: ");
+	scanf("%i", &n);
+	int temp = IsPrime(n);
+	if(temp)
+	{
+		printf("n is prime");
+	}
+	else
+	{
+		printf("n is not prime");
+	}
+}
