6662ebf5dbdeb4ea347f8abeb552dc411e4ee436
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-05-28 17:25:44 +1200

    Update homework 43 bai 2.c

--- homework 43 bai 2.c
+++ homework 43 bai 2.c
@@ -11,9 +11,13 @@ void FindS(int n)
 }
 int IsPrime(int n)
 {
-	for (int i = 2; i < sqrt(n); i++)
+	if(n == 2)
 	{
-		if(n%i == 0)
+		return 1;
+	}
+	for (int i = 2; i < sqrt(n)+1; i++)
+	{
+		if (n % i == 0)
 		{
 			return 0;
 		}
