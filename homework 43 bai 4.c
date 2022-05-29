7027cc0a55602810eaa6eb23f3a2eaf0b4bbc0ec
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-05-29 16:43:40 +1200

    Update homework 43 bai 4.c

--- homework 43 bai 4.c
+++ homework 43 bai 4.c
@@ -10,20 +10,30 @@ void input(int *num, int *den)
 		scanf("%i", den);
 	} while (*den == 0);
 }
+int GCD(int a, int b)
+{
+	if (a == 0)
+	{
+		return b;
+	}
+	do
+	{
+		if (a > b)
+		{
+			a -= b;
+		}
+		else if (a < b)
+		{
+			b -= a;
+		}
+	} while (a != b);
+	return a;
+}
 void simplify(int *num, int *den)
 {
 	int numTemp = abs(*num), denTemp = abs(*den);
-	do
-	{
-		if (numTemp > denTemp)
-		{
-			numTemp -= denTemp;
-		}
-		else if (numTemp < denTemp)
-		{
-			denTemp -= numTemp;
-		}
-	} while (numTemp != denTemp);
+	numTemp = GCD(numTemp, denTemp);
+
 	if (*den < 0)
 	{
 		numTemp *= -1;
@@ -36,17 +46,18 @@ void display(int num, int den)
 {
 	int simpnum = num, simpden = den;
 	simplify(&simpnum, &simpden);
-	printf("%i/%i\n=\n%i/%i\n",num,den,simpnum,simpden);
+	printf("%i/%i\n=\n%i/%i\n", num, den, simpnum, simpden);
 }
 int main()
 {
 	int num, den;
 	char answer;
-	do{
+	do
+	{
 		input(&num, &den);
 		display(num, den);
-		printf("Another run <y/n>?");
-		scanf("%c",&answer);
-		scanf("%c",&answer);
-	}while(answer == 'y');
+		printf("Another run <y/n>? ");
+		scanf("%c", &answer);
+		scanf("%c", &answer);
+	} while (answer == 'y');
 }
